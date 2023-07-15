a2c2ec2ce732590dd921db4331d3e6df5fe29bcc
Jesse Katsumata <jesse.katsumata@gmail.com>
2023-07-15 07:08:43 +0000

    chore: remove built file

--- 1-getting-started/hello_cargo/target/debug/deps/hello_cargo-97c804fb0741aa2c.d
+++
@@ -1,5 +0,0 @@
-/workspaces/rust-playground/1-getting-started/hello_cargo/target/debug/deps/hello_cargo-97c804fb0741aa2c: src/main.rs
-
-/workspaces/rust-playground/1-getting-started/hello_cargo/target/debug/deps/hello_cargo-97c804fb0741aa2c.d: src/main.rs
-
-src/main.rs:
--- 1-getting-started/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/workspaces/rust-playground/1-getting-started/hello_cargo/target/debug/hello_cargo: /workspaces/rust-playground/1-getting-started/hello_cargo/src/main.rs
